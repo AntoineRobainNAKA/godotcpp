7804704a4263a97be8500340d995b3d585c34bb4
Matos <18725330+tosmatos@users.noreply.github.com>
2024-11-11 20:41:47 +0100

    Ajoute commentaires GridWorldNode.cpp

--- src/GridWorldNode.cpp
+++ src/GridWorldNode.cpp
@@ -22,7 +22,12 @@ GridWorldNode::~GridWorldNode() {
     columns = 0;
 }
 
+// ps : le résultat retourné n'est pas bon (c'est évident si on regarde)
+// C'est un problème dans le gridworld directement
+// pas encore trouvé la solution ¯\_(ツ)_/¯
 String GridWorldNode::launch_policy_iteration(const int rows, const int columns) {
+    // Pas trouvé le moyen de passer la taille en paramètre
+    // C'est une connerie avec le template qui existe des const
     GridWorld<5, 5> gridworld;
     auto [pi_gridworld, value_function_gridworld] = policy_iteration(gridworld, 0.999f, 0.001f);
 
@@ -35,9 +40,11 @@ String GridWorldNode::launch_policy_iteration(const int rows, const int columns)
         ss << "V(s=" << s << ") = " << value_function_gridworld[s] << std::endl;
     }
 
+    // Godot veut la String (godot::String) et pas std::string
     return String(ss.str().c_str());
 }
 
+// Obligé de mettre _process sinon ça compile pas
 void GridWorldNode::_process(double delta) {
     //nothing for now I guess
 }
