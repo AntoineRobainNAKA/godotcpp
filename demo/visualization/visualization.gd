d01e7604054f0c9863dec31f0c776729777700e0
Matos <18725330+tosmatos@users.noreply.github.com>
2024-11-21 21:57:12 +0100

    Ajoute scene visualisation, environment_container

--- demo/visualization/visualization.gd
+++ demo/visualization/visualization.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var environment_dropdown: OptionButton = %EnvironmentDropdown
 @onready var algorithm_dropdown: OptionButton = %AlgorithmDropdown
+@onready var grid_visualizer: GridVisualizer = %GridVisualizer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for environment in Globals.Environments:
@@ -14,3 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
